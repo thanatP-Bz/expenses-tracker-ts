cb6c7e0c4d237872b26a1753f3d9d1a3608cfb45
Thanat <bztzn512@gmail.com>
2023-05-10 19:37:31 -0400

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header";
 import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
@@ -7,7 +6,6 @@ import AddTransition from "./components/AddTransition";
 function App() {
   return (
     <>
-      <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
